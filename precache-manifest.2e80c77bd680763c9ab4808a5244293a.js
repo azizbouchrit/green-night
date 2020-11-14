277fb816f37efc14a1012e8e627def51f8b98a39
azizbouchrit <medazizbouchrit@gmail.com>
2020-11-14 07:15:20 +0100

    Updates

---
+++ precache-manifest.2e80c77bd680763c9ab4808a5244293a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbdd1237c48e6ad47482aa6a9d46439",
+    "url": "/green-night/index.html"
+  },
+  {
+    "revision": "1cee94ecabe3a4844ffe",
+    "url": "/green-night/static/css/main.c5ea059b.chunk.css"
+  },
+  {
+    "revision": "66181cca1776951dcb93",
+    "url": "/green-night/static/js/2.3e4e33ef.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/green-night/static/js/2.3e4e33ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cee94ecabe3a4844ffe",
+    "url": "/green-night/static/js/main.56e86128.chunk.js"
+  },
+  {
+    "revision": "7ad4b1589eab71cbee3b",
+    "url": "/green-night/static/js/runtime-main.48a71fe4.js"
+  }
+]);
